62daee6e8362e0d2cacc1d32b49c8038af2d48d4
CZrich <czrichard@gmail.com>
2023-12-11 17:04:59 -0500

    avance de labs

---
+++ lab02/probabilidad.m
@@ -0,0 +1,21 @@
+clear all;
+%res= factorial(5);
+%display("el factorial es");
+%display(res);
+%datos=[20 30 45 60 41 55 8];
+%combi=nchoosek(5,2)
+%display("la combinatoria es");
+%display(combi);
+v=['a','v','e'];
+display("la permutacion  es ")
+display(perms(v));
+display("el aletorio es ");
+display(rand(3,2));
+display(randi(5));% devuelve valores enteros
+%rand(5)
+%rand(3,2)
+
+ %metodo redondeado
+display(round( rand() ));
+
+
---
+++ lab02/problema_cinco.m
@@ -0,0 +1,6 @@
+clear all;
+
+% ciclo for
+for i=1:10
+  display(round(rand()));
+endfor
